f7846214787097207053ac69a7679a57e2f46843
Josh Archer <jarcher@greenriver.edu>
2023-10-02 15:08:26 -0700

    Added class files.

---
+++ in-class/github/2_variables_data_types/3_element_selection/example/script.js
@@ -0,0 +1,11 @@
+//1st step is to select HTML elements on the page
+let spanFirst = document.querySelector("#first");
+let spanLast = document.querySelector("#last");
+let spanMiddle = document.querySelector("#middle");
+let spanNickname = document.querySelector("#nickname");
+
+//update the text in the HTML elements
+spanFirst.innerHTML = "Josh";
+spanLast.innerHTML = "Archer";
+spanMiddle.innerHTML = "B";
+spanNickname.innerHTML = "Teach";
