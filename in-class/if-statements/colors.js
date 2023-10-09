04bfc6bcf1f63a1f38e94a8b08808e62507bab22
Josh Archer <jarcher@greenriver.edu>
2023-10-09 14:37:29 -0700

    Added an example of if-statements logic.

---
+++ in-class/if-statements/colors.js
@@ -0,0 +1,40 @@
+//read in rgb values (as strings)
+let r = parseInt(prompt("Enter red component"));
+let g = parseInt(prompt("Enter green component"));
+let b = parseInt(prompt("Enter blue component"));
+
+//print out the color to see if the code above works!
+console.log(`The color you entered is (${r},${g},${b})`);
+
+//check for valid color component ranges
+let redValid = r >= 0 && r <= 255;
+let greenValid = g >= 0 && g <= 255;
+let blueValid = b >= 0 && b <= 255;
+
+//is the color valid or not?
+if (redValid && greenValid && blueValid)
+{
+    //figure out which color
+    if (r === 0 && g === 0 && b === 0)
+    {
+        console.log("The color is black");
+    }
+    else if (r === 255 && g === 255 && b === 255)
+    {
+        console.log("The color is white");
+    }
+    else if (r === 255 && g === 0 && b === 255)
+    {
+        console.log("The color is purple/magenta");
+    }
+    else
+    {
+        console.log("I can't figure out this color?!?!");
+    }
+}
+else
+{
+    //not valid
+    console.log("Please enter color components in the range [0,255]");
+}
+
--- in-class/intro-to-js/my-script.js
+++ in-class/intro-to-js/my-script.js
@@ -1,3 +1,10 @@
+/*
+    Josh Archer
+    10/5/2023
+    my-script.js
+    This is a 1st file working with JS.
+ */
+
 //select the <p> element above and put in the "para" variable
 let para = document.querySelector('p');
 para.innerHTML = 'Welcome to my <strong>AWESOME</strong> site!';
