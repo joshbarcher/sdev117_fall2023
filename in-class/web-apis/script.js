3b46cb02348a40f78582a889bb651a385438c3de
Josh Archer <jarcher@greenriver.edu>
2023-11-29 13:30:25 -0800

    Added another request to our practice with web apis.

--- in-class/web-apis/script.js
+++ in-class/web-apis/script.js
@@ -1,11 +1,47 @@
 //wait until page loads
-window.onload = () => {
+window.onload = async () => {
     //add a click event handler to our button
     let button = document.querySelector("button");
     button.onclick = loadRandomCat;
+
+    let url = "https://api.thecatapi.com/v1/breeds";
+    let config = {
+        method: "get",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "cab8736c-d9ae-481e-8e20-4fe581"
+        }
+    };
+
+    let response = await fetch(url, config);
+    let data = await response.json();
+    showBreeds(data);
 };
 
-function loadRandomCat()
+function showBreeds(breedsArray)
+{
+    //display breeds to the user
+    console.log(breedsArray);
+
+    //loop over each of our breeds and add to the breeds-section
+    let breedsSection = document.querySelector("#breeds");
+    for (let i = 0; i < breedsArray.length; i++)
+    {
+        let breed = breedsArray[i];
+
+        //add a div with the breed name inside
+        let div = document.createElement("div");
+        let header = document.createElement("h3");
+
+        header.textContent = breed.name;
+
+        div.appendChild(header);
+        breedsSection.appendChild(div);
+    }
+}
+
+async function loadRandomCat()
 {
     //configure a request
     let url = "https://api.thecatapi.com/v1/images/search";
@@ -19,7 +55,7 @@ function loadRandomCat()
     };
 
     //make a request using fetch!
-    fetch(url, config)
+    /*fetch(url, config)
         .then(function(response) {
             console.log(response);
 
@@ -32,13 +68,12 @@ function loadRandomCat()
             //we got back an array with a single element
             //access that element at index zero
             displayImage(data[0]);
-        });
+        });*/
 
     //we can also use the async/await keywords
-    //let response = await fetch(url, config);
-    //let data = await promise.json();
-
-
+    let response = await fetch(url, config);
+    let data = await response.json();
+    displayImage(data[0]);
 }
 
 function displayImage(catData)
