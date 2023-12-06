2159c644b446265f67b3b04a33b6b77a58e3be57
Josh Archer <jarcher@greenriver.edu>
2023-12-06 13:52:04 -0800

    Added a custom validation using JS.

--- in-class/forms/validation.js
+++ in-class/forms/validation.js
@@ -31,7 +31,48 @@ function validateForm(event)
     console.log("User rating is", rating);
     console.log(title, author, pages, release, isbn);
 
+    pages = parseInt(pages);
+
+    //custom validation
+    let errors = 0;
+
+    if (validateStringField(title, "title-error", 100)) errors++;
+    if (validateStringField(author, "author-error", 30)) errors++;
+
+    // (make sure it's a number and in the range [0,2500]
+    if (Number.isNaN(pages) || pages < 0 || pages > 2500)
+    {
+        //bad value (show the error next to the pages box)
+        document.querySelector("#pages-error").style.display = "inline";
+        errors++;
+    }
+    else
+    {
+        //good value
+        document.querySelector("#pages-error").style.display = "none";
+    }
+
     //after our validation, we need to submit the form
     //if everything is all right...
+    if (errors === 0)
+    {
+        //programmatically submit the form
+        document.forms["book-entry"].submit();
+    }
+}
 
+function validateStringField(stringValue, errorSpanId, maxCharacters)
+{
+    if (stringValue.length === 0 || stringValue.length > maxCharacters)
+    {
+        //bad value
+        document.querySelector(`#${errorSpanId}`).style.display = "inline";
+        return true; //yes there was an error
+    }
+    else
+    {
+        //good value
+        document.querySelector(`#${errorSpanId}`).style.display = "none";
+        return false; //no there was not an error
+    }
 }
